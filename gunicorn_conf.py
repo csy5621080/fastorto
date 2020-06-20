13ed134f49f3b6e91c4ac08cb53c7f85a7d8af3b
csy5621080 <499938136@qq.com>
2020-06-20 13:02:03 +0800

    路由自动加载兼容windows

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,3 +1,6 @@
 bind = '0.0.0.0:8050'
 workers = 4
 worker_class = 'uvicorn.workers.UvicornWorker'
+
+accesslog = "/var/log/gunicorn_access.log"  # 访问日志文件
+errorlog = "/var/log/gunicorn_error.log"  # 错误日志文件
